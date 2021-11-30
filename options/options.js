243ac6d7622f73ab72e2e0c87bbc8b43da5e013e
Crowdbotics <team@crowdbotics.com>
2021-11-30 04:34:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "passion_finder_101_32195",
+  url: "https://passion_finder_101_32195.botics.co",
+  api: "https://passion_finder_101_32195.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
